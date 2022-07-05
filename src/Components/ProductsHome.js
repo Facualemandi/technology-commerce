2a3ccabadd975fa9fba1ddbc9759a22bcbebe8d9
Facundo Alemandi <facuinstituto10@gmail.com>
2022-07-04 22:14:43 -0300

    Agregando un Loader para products Home, hasta ca todo funcina bien

--- src/Components/ProductsHome.js
+++ src/Components/ProductsHome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../Context/authContext";
 import { useProducts } from "../Hooks/useProducts";
+import Loader from "../Loader/Loader";
 import { StyleHomeProducts } from "../Styles/StyleHomeProducts";
 
 const {
@@ -21,64 +22,68 @@ const ProductsHome = () => {
   return (
     <>
       <Main>
-        <Parragraph>Elegí la mejor Notebook para trabajar o jugar!</Parragraph>
-        <SectionContianer>
-          {notebook.map((note) => (
-            <>
-              <TheNavLink to={`/Notebook/${note.name}`}>
-                <Section onClick={() => handleProduct(note)}>
-                  <SectionImg>
-                    <Img alt={note.name} src={note.img} />
-                  </SectionImg>
-                  <Price>${note.price}</Price>
-                  <NameProduct>{note.name}</NameProduct>
-                </Section>
-              </TheNavLink>
-            </>
-          ))}
-        </SectionContianer>
+        {loaging && <Loader />}
+        
+        {!loaging && (
+          <>
+            <Parragraph>
+              Elegí la mejor Notebook para trabajar o jugar!
+            </Parragraph>
+            <SectionContianer>
+              {notebook.map((note) => (
+                <>
+                  <TheNavLink to={`/Notebook/${note.name}`}>
+                    <Section onClick={() => handleProduct(note)}>
+                      <SectionImg>
+                        <Img alt={note.name} src={note.img} />
+                      </SectionImg>
+                      <Price>${note.price}</Price>
+                      <NameProduct>{note.name}</NameProduct>
+                    </Section>
+                  </TheNavLink>
+                </>
+              ))}
+            </SectionContianer>
 
-        {/* //////////////////////////////////// */}
+            <Parragraph>Mejora el rendimiento de tus juego</Parragraph>
+            <SectionContianer>
+              {placas.map((product) => (
+                <>
+                  {!loaging && (
+                    <TheNavLink to={`/PlacaDeVideo/${product.name}`}>
+                      <Section onClick={() => handleProduct(product)}>
+                        <SectionImg>
+                          <Img alt={product.name} src={product.img} />
+                        </SectionImg>
+                        <Price>${product.price}</Price>
+                        <NameProduct>{product.name}</NameProduct>
+                      </Section>
+                    </TheNavLink>
+                  )}
+                </>
+              ))}
+            </SectionContianer>
 
-        <Parragraph>Mejora el rendimiento de tus juego</Parragraph>
-        <SectionContianer>
-          {placas.map((product) => (
-            <>
-              {!loaging && (
-                <TheNavLink to={`/PlacaDeVideo/${product.name}`}>
-                  <Section onClick={() => handleProduct(product)}>
-                    <SectionImg>
-                      <Img alt={product.name} src={product.img} />
-                    </SectionImg>
-                    <Price>${product.price}</Price>
-                    <NameProduct>{product.name}</NameProduct>
-                  </Section>
-                </TheNavLink>
-              )}
-            </>
-          ))}
-        </SectionContianer>
-
-        {/* ////////////////////////////////////////// */}
-
-        <Parragraph>Resolución al Máximo!</Parragraph>
-        <SectionContianer>
-          {monitores.map((product) => (
-            <>
-              {!loaging && (
-                <TheNavLink to={`/Monitor/${product.name}`}>
-                  <Section onClick={() => handleProduct(product)}>
-                    <SectionImg>
-                      <Img alt={product.name} src={product.img} />
-                    </SectionImg>
-                    <Price>${product.price}</Price>
-                    <NameProduct>{product.name}</NameProduct>
-                  </Section>
-                </TheNavLink>
-              )}
-            </>
-          ))}
-        </SectionContianer>
+            <Parragraph>Resolución al Máximo!</Parragraph>
+            <SectionContianer>
+              {monitores.map((product) => (
+                <>
+                  {!loaging && (
+                    <TheNavLink to={`/Monitor/${product.name}`}>
+                      <Section onClick={() => handleProduct(product)}>
+                        <SectionImg>
+                          <Img alt={product.name} src={product.img} />
+                        </SectionImg>
+                        <Price>${product.price}</Price>
+                        <NameProduct>{product.name}</NameProduct>
+                      </Section>
+                    </TheNavLink>
+                  )}
+                </>
+              ))}
+            </SectionContianer>
+          </>
+        )}
       </Main>
     </>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,25 +2,17 @@ import React from "react";
 import Categories from "../../Components/Categories";
 import Marks from "../../Components/Marks";
 import Nav from "../../Components/Nav";
-import Notebooks from "../../Components/Notebooks";
 import SliderShow from "../../Components/SliderShow";
-import PlacasDeVideo from "../../Components/PlacasDeVideo";
-import Monitores from "../../Components/Monitores";
 import ProductsHome from "../../Components/ProductsHome";
 
 const Home = () => {
-
-
   return (
     <>
       <Nav />
       <SliderShow />
       <Marks />
       <Categories />
-      <ProductsHome/>
-      {/* <Notebooks />
-      <PlacasDeVideo />
-      <Monitores /> */}
+      <ProductsHome />
     </>
   );
 };
